ace0b4c6b21bddfc92a3b5e96b559bc057455eda
Dale <dellryuzi@gmail.com>
2023-06-01 13:59:35 +0700

    refactor: clean structure

---
+++ AwesomeProject/src/navigations/root.tsx
@@ -0,0 +1,22 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import PlayScreen from '../screens/PlayScreen';
+import PushScreen from '../screens/PushScreen';
+import HomeScreen from '../screens/HomeScreen';
+const Stack = createStackNavigator();
+
+export default function RootStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" headerMode="none">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'iShow'}}
+        />
+        <Stack.Screen name="Play" component={PlayScreen} />
+        <Stack.Screen name="Push" component={PushScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
